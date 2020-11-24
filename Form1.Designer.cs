8679d6db9ac998d1b29c5b084c020cbbfbe88eb9
Stephen Seed <stephenrseed@hotmail.com>
2020-11-23 16:44:34 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace WGH_Notification_Engine
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblToPeople = new System.Windows.Forms.Label();
+            this.lblSendingUser = new System.Windows.Forms.Label();
+            this.lblMessageType = new System.Windows.Forms.Label();
+            this.lblMessageText = new System.Windows.Forms.Label();
+            this.lblGameID = new System.Windows.Forms.Label();
+            this.lblMessagePriority = new System.Windows.Forms.Label();
+            this.textBoxWhoTo = new System.Windows.Forms.TextBox();
+            this.textBoxSender = new System.Windows.Forms.TextBox();
+            this.textBoxMessageType = new System.Windows.Forms.TextBox();
+            this.textBoxMessageText = new System.Windows.Forms.TextBox();
+            this.textBoxGameID = new System.Windows.Forms.TextBox();
+            this.textBoxMessagePriority = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblToPeople
+            // 
+            this.lblToPeople.AutoSize = true;
+            this.lblToPeople.Location = new System.Drawing.Point(28, 59);
+            this.lblToPeople.Name = "lblToPeople";
+            this.lblToPeople.Size = new System.Drawing.Size(276, 15);
+            this.lblToPeople.TabIndex = 0;
+            this.lblToPeople.Text = "Who To (Comma Seperated, e-mail, phone, UserID)";
+            // 
+            // lblSendingUser
+            // 
+            this.lblSendingUser.AutoSize = true;
+            this.lblSendingUser.Location = new System.Drawing.Point(28, 108);
+            this.lblSendingUser.Name = "lblSendingUser";
+            this.lblSendingUser.Size = new System.Drawing.Size(234, 15);
+            this.lblSendingUser.TabIndex = 1;
+            this.lblSendingUser.Text = "Sender (Numeric) or \"System\" (Mandatory)";
+            // 
+            // lblMessageType
+            // 
+            this.lblMessageType.AutoSize = true;
+            this.lblMessageType.Location = new System.Drawing.Point(28, 157);
+            this.lblMessageType.Name = "lblMessageType";
+            this.lblMessageType.Size = new System.Drawing.Size(201, 15);
+            this.lblMessageType.TabIndex = 2;
+            this.lblMessageType.Text = "Message Type (Numeric, Mandatory)";
+            // 
+            // lblMessageText
+            // 
+            this.lblMessageText.AutoSize = true;
+            this.lblMessageText.Location = new System.Drawing.Point(28, 206);
+            this.lblMessageText.Name = "lblMessageText";
+            this.lblMessageText.Size = new System.Drawing.Size(171, 15);
+            this.lblMessageText.TabIndex = 3;
+            this.lblMessageText.Text = "Message Text (String, Optional)";
+            // 
+            // lblGameID
+            // 
+            this.lblGameID.AutoSize = true;
+            this.lblGameID.Location = new System.Drawing.Point(28, 255);
+            this.lblGameID.Name = "lblGameID";
+            this.lblGameID.Size = new System.Drawing.Size(157, 15);
+            this.lblGameID.TabIndex = 4;
+            this.lblGameID.Text = "Game ID (numeric, optional)";
+            // 
+            // lblMessagePriority
+            // 
+            this.lblMessagePriority.AutoSize = true;
+            this.lblMessagePriority.Location = new System.Drawing.Point(28, 304);
+            this.lblMessagePriority.Name = "lblMessagePriority";
+            this.lblMessagePriority.Size = new System.Drawing.Size(212, 15);
+            this.lblMessagePriority.TabIndex = 5;
+            this.lblMessagePriority.Text = "Message Priority (1,2 or 3 - Mandatory)";
+            // 
+            // textBoxWhoTo
+            // 
+            this.textBoxWhoTo.Location = new System.Drawing.Point(304, 56);
+            this.textBoxWhoTo.Name = "textBoxWhoTo";
+            this.textBoxWhoTo.Size = new System.Drawing.Size(100, 23);
+            this.textBoxWhoTo.TabIndex = 6;
+            // 
+            // textBoxSender
+            // 
+            this.textBoxSender.Location = new System.Drawing.Point(304, 105);
+            this.textBoxSender.Name = "textBoxSender";
+            this.textBoxSender.Size = new System.Drawing.Size(100, 23);
+            this.textBoxSender.TabIndex = 7;
+            // 
+            // textBoxMessageType
+            // 
+            this.textBoxMessageType.Location = new System.Drawing.Point(304, 154);
+            this.textBoxMessageType.Name = "textBoxMessageType";
+            this.textBoxMessageType.Size = new System.Drawing.Size(100, 23);
+            this.textBoxMessageType.TabIndex = 8;
+            // 
+            // textBoxMessageText
+            // 
+            this.textBoxMessageText.Location = new System.Drawing.Point(304, 203);
+            this.textBoxMessageText.Name = "textBoxMessageText";
+            this.textBoxMessageText.Size = new System.Drawing.Size(100, 23);
+            this.textBoxMessageText.TabIndex = 9;
+            // 
+            // textBoxGameID
+            // 
+            this.textBoxGameID.Location = new System.Drawing.Point(304, 252);
+            this.textBoxGameID.Name = "textBoxGameID";
+            this.textBoxGameID.Size = new System.Drawing.Size(100, 23);
+            this.textBoxGameID.TabIndex = 10;
+            // 
+            // textBoxMessagePriority
+            // 
+            this.textBoxMessagePriority.Location = new System.Drawing.Point(304, 301);
+            this.textBoxMessagePriority.Name = "textBoxMessagePriority";
+            this.textBoxMessagePriority.Size = new System.Drawing.Size(100, 23);
+            this.textBoxMessagePriority.TabIndex = 11;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(55, 379);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 12;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 15);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Prefix each with \"email\", \"text\" or \"app\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.textBoxMessagePriority);
+            this.Controls.Add(this.textBoxGameID);
+            this.Controls.Add(this.textBoxMessageText);
+            this.Controls.Add(this.textBoxMessageType);
+            this.Controls.Add(this.textBoxSender);
+            this.Controls.Add(this.textBoxWhoTo);
+            this.Controls.Add(this.lblMessagePriority);
+            this.Controls.Add(this.lblGameID);
+            this.Controls.Add(this.lblMessageText);
+            this.Controls.Add(this.lblMessageType);
+            this.Controls.Add(this.lblSendingUser);
+            this.Controls.Add(this.lblToPeople);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblToPeople;
+        private System.Windows.Forms.Label lblSendingUser;
+        private System.Windows.Forms.Label lblMessageType;
+        private System.Windows.Forms.Label lblMessageText;
+        private System.Windows.Forms.Label lblGameID;
+        private System.Windows.Forms.Label lblMessagePriority;
+        private System.Windows.Forms.TextBox textBoxWhoTo;
+        private System.Windows.Forms.TextBox textBoxSender;
+        private System.Windows.Forms.TextBox textBoxMessageType;
+        private System.Windows.Forms.TextBox textBoxMessageText;
+        private System.Windows.Forms.TextBox textBoxGameID;
+        private System.Windows.Forms.TextBox textBoxMessagePriority;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WGH_Notification_Engine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        public string PeopleToContact; // List of people to contact with a message (At least one mandatory)
+        public int FromUserID; // User ID for the person that is sending the message (Mandatory). If userid = "0" then SYSTEM sent the message.
+        public int MessageType; // They type of message that is being sent (Mandatory):
+                                // 1.	Custom message(must provide message text)
+                                // 2.	New user invite
+                                // 3.	Share on social media - player or manager instigated
+                                // 4.	Share game info to existing users
+                                // 5.	Player Cancels their acceptance to play
+                                // 6.	Ask player if they want to add User to Friend List(Not a two way relationship)
+                                // 7.	Player accepts game
+                                // 8.	Player accepts game(notify manager) & player wants to share game – perhaps this is two calls to the module from the instigating routine.
+                                // 9.	Manager cancels game – notify player(s)
+        public string MessageText; // Free form text that will be used (Mandatory for custom messages)
+        public int GameID; // ID for the game information stored on file. Only required when game info needed.
+        public int MessagePriority; // The priority of the message will determinte the type of message sent.
+                                    // Lower priority messages will be included. I.e Pri 1 will include Popup, Toast & In-App messsge (Mandatory). 
+                                    // 1.   Popup
+                                    // 2.   Toast - Red circle notification on app icon
+                                    // 3.   In-App message
+// Testing Github
+        public string NotificationMessage(string PeopleToContact, int FromUserID, int MessageType, string MessageText, int GameID, int MessagePriority)
+        {
+            // We need to loop through each person that is due to receive the message.
+            // People to contact can be multiple users with comma seperators.
+            // Can be an e-mail address, Telephone number or User ID.
+            // To be sure we use the right type, we will require a prefix for each user. Using the following guidance:
+            // TEL nnnnnnnnn
+            // EMAIL xxxxxx@xxxxx.xxx
+            // ID nnnnnn
+
+            // This code splits the entry in to seperate elements of an array.
+            string[] peopleToContactArray;
+      
+
+
+            peopleToContactArray = PeopleToContact.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            var message = new StringBuilder();
+
+            // loop through each contact and send them a message.
+            for (int i = 0; i < peopleToContactArray.Length; i++)
+            {
+                message = new StringBuilder();
+                string personToContact = peopleToContactArray[i];
+                string[] contactTypeArray;
+                contactTypeArray = personToContact.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string contactType = contactTypeArray[0].ToUpper();
+                string contactDetails = contactTypeArray[1];
+                bool addToUser = true;
+                switch (MessageType)
+                {
+                    case 1: // Custom Message
+
+                        message.AppendLine("This is a custom message (type 1)");
+                        message.AppendLine("Message Text : " + MessageText);
+                        break;
+
+                    case 2: // New User Invite.
+                        message.AppendLine("New User Invite (type 2)");
+                        message.AppendLine("Message Text : You have been invited to join.");
+                        break;
+
+                    case 3: // Share on social Media
+                            // do this
+                        message.AppendLine("User " + FromUserID + " has decided to share game " + GameID + " on social media.");
+                        addToUser = false;
+                        break;
+
+                    case 4: // Share game info to existing user
+                        message.AppendLine("This is a game available message (type 4)");
+                        if (FromUserID == 0)
+                        {
+                            message.AppendLine("SYSTEM generated message : A game is available that matches your requirements. See game " + GameID);
+                        }
+                        else
+                        {
+                            message.AppendLine("Message Text : A game is available that matches your requirements. See game " + GameID + " from User " + FromUserID);
+                        }
+                        break;
+
+                    case 5: // Player cancels their acceptance to play
+
+                        message.AppendLine("Player Cancels and acceptance (type 5)");
+                        message.AppendLine("Message Text : Player " + FromUserID + " has cancelled invite to game " + GameID + " they previously accepted.");
+                        break;
+
+                    case 6: // Ask player if they want to add player to friend list.
+                        message.AppendLine("Ask player if they want to add user to friend list. (type 6)");
+                        message.AppendLine("Message Text : Do you want to add player to your friend list.");
+                        // do this
+                        break;
+                    case 7: // Player accepts game - let manager know
+                        message.AppendLine("Player has accepted a game. (type 7)");
+                        message.AppendLine("Message Text : Player " + FromUserID + " has accepted game - " + GameID);
+                        break;
+                    case 8: // Player accepts game - let manager know & player wants to share on social media
+                        message.AppendLine("Player has accepted a game & social media post (type 8)");
+                        message.AppendLine("Message Text : Player " + FromUserID + " has accepted game - " + GameID + " and has chosen to post on social media.");
+                        // do this
+                        break;
+                    case 9: // Manager cancels game - notify player(s)
+                        message.AppendLine("Manager cancels a game. (type 9)");
+                        message.AppendLine("Message Text : Manager " + FromUserID + " has canceled the game " + GameID);
+                        // do this
+                        break;
+                    default: // This should not occur and is an error - use exception catching.
+                        message.AppendLine("Error - Message type does not exist (type " + MessageType + ")");
+                        message.AppendLine("Message Text : Message type does not exist.");
+                        addToUser = false;
+                        // do this
+                        break;
+                }
+
+
+                if (addToUser)
+                {
+                    message.Append("Sent to user : " + contactDetails);
+
+                    // What type of contact will we make. Phone, e-mail or app messaging.
+
+                    switch (contactType)
+                    {
+                        case "EMAIL":
+                            message.AppendLine(" using : EMAIL");
+
+                            // See here on how to send an automated e-mail
+                            // https://www.c-sharpcorner.com/article/sending-email-using-c-sharp/
+
+                            break;
+
+                        case "TEXT":
+                            message.AppendLine(" using : TEXT");
+
+                            // see here how to send an automated text message
+                            // https://www.c-sharpcorner.com/article/send-text-message-to-cell-phones-from-a-C-Sharp-application/
+
+                            break;
+
+                        case "APP":
+                            message.AppendLine(" using : APP");
+                            break;
+
+                        default:
+                            message.AppendLine(" using : APP - no method selected for sending defaults to APP.");
+                            break;
+
+                    }
+                }
+                    message.AppendLine("From User : " + FromUserID);
+                    switch (MessagePriority)
+                    {
+                        case 1: // Popup message, Toast & in app message
+                            message.AppendLine("Message Priority : 1");
+                            message.AppendLine("Pop up Message - NOT Delivered");
+                            message.AppendLine("Toast Message - NOT Delivered");
+                            message.AppendLine("In App message - NOT delivered");
+                            break;
+                        case 2: // Toast - red circle notification & in app message
+                            message.AppendLine("Message Priority : 2");
+                            message.AppendLine("Toast Message - NOT Delivered");
+                            message.AppendLine("In App message - NOT delivered");
+                        break;
+                        case 3: // In app message only
+                            message.AppendLine("Message Priority : 3");
+                            message.AppendLine("In App message - NOT delivered");
+                        break;
+                        default:
+                            message.AppendLine("Message Priority : 3 - No message priority selected and defaults to 3");
+                            message.AppendLine("In App message - NOT delivered");
+                        break;
+                    }             
+                // using UTC to ensure consistency accross timezones.
+
+                message.AppendLine(DateTime.UtcNow.ToString());
+
+                string path = @"UserMessages.txt";
+                string fullPath = Path.GetFullPath(path);
+
+
+                SaveAppMessage(message.ToString(), personToContact);
+
+
+                // TODO: This can be deleted once implemented fully with the file structure.
+                message.AppendLine("File : " + fullPath);
+                MessageBox.Show(message.ToString());
+
+            }
+            return message.ToString();
+        }
+
+        // Show an in app message only
+
+        public string SaveAppMessage(string messageToSave,string personToContact)
+        {
+            // open message file & read user record - personToContact will only be used with a DB as a key to a record.
+            // file will need to be created during the installation process. It will be created in the default location "UserMessages.txt"
+
+            string path = @"UserMessages.txt";
+
+            // TODO: Need to check if the file exists before doing anything. If it does not exist, then it needs to be created.
+            try
+            {
+                using (StreamWriter sr = File.AppendText(path))
+                {
+                    sr.WriteLine(messageToSave);
+                    sr.Close();
+                }
+            }
+            catch(System.IO.DirectoryNotFoundException ex)
+            {
+                string message = "Directory Not Found. Create First." + ex;
+                MessageBox.Show(message.ToString());
+            }
+            catch(System.IO.FileNotFoundException ex)
+            {
+                string message = "File Not Found. Create First." + ex;
+                MessageBox.Show(message.ToString());
+            }
+            return messageToSave.ToString();
+        }
+
+
+        // Show a an in app message AND a toast notification. (Red circle on app icon).
+
+        public string SaveAppMessage(string messageToSave)
+        {
+            // open message file & read user record
+
+            // add message to file
+
+            // write record
+
+            // close file
+
+            return messageToSave.ToString();
+        }
+        // Mobile phone Popup message AND in app message AND a toast notification. (Red circle on app icon).
+
+
+
+
+
+
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            PeopleToContact = textBoxWhoTo.Text;
+            if (textBoxSender.Text.ToUpper() == "SYSTEM")
+            {
+                textBoxSender.Text = "0";
+            }
+            FromUserID = int.Parse(textBoxSender.Text);
+            MessageType = int.Parse(textBoxMessageType.Text);
+            MessageText = textBoxMessageText.Text;
+            GameID = int.Parse(textBoxGameID.Text);
+            MessagePriority = int.Parse(textBoxMessagePriority.Text);
+            NotificationMessage(PeopleToContact, FromUserID, MessageType, MessageText, GameID, MessagePriority);
+
+
+        }
+    }
+}
